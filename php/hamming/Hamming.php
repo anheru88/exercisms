dce4c903f78322df153be0d2ac29f4a03cf2d00c
Angel Jimenez <ajimenez@digitalcaribe.com.co>
2023-05-12 10:36:42 -0500

    New iteration for Hamming

--- php/hamming/Hamming.php
+++ php/hamming/Hamming.php
@@ -26,18 +26,14 @@ declare(strict_types=1);
 
 function distance(string $strandA, string $strandB): int
 {
-    $sum = 0;
 
-    if (strlen($strandA) === strlen($strandB)) {
-        for ($i = 0; $i < strlen($strandA); $i++) {
-            if ($strandA[$i] !== $strandB[$i]) {
-                $sum++;
-            }
-        }
-    } else {
+    if (strlen($strandA) !== strlen($strandB)) {
         throw new InvalidArgumentException('DNA strands must be of equal length.');
     }
 
-    return $sum;
+    return count(array_diff_assoc(
+        str_split($strandA),
+        str_split($strandB)
+    ));
 
 }
